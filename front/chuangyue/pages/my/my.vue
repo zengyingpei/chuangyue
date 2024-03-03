a0ab049263c9d10e4a75c57ec507bff3d709a261
zyp <2075504518@qq.com>
2024-03-03 21:14:20 +0800

    创建项目

---
+++ front/chuangyue/pages/my/my.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		页面内容
+	</view>
+	<div>asdasds</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
