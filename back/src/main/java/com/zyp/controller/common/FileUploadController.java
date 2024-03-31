1b914ba9c473623dc049cade763991a696957d34
zyp <2075504518@qq.com>
2024-03-31 17:57:15 +0800

    MiniO文件上传

--- back/src/main/java/com/zyp/controller/common/FileUploadController.java
+++ back/src/main/java/com/zyp/controller/common/FileUploadController.java
@@ -1,7 +1,12 @@
 package com.zyp.controller.common;
 
+import com.zyp.exception.UploadErrorException;
 import com.zyp.pojo.Result;
+import com.zyp.properties.MinioProperties;
 import com.zyp.utils.AliOssUtil;
+import com.zyp.utils.MinioUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +17,27 @@ import java.io.IOException;
 import java.util.UUID;
 
 @RestController
+@Slf4j
 public class FileUploadController {
+    @Autowired
+    private MinioProperties minioProperties;
+
+    /**
+     * @ description 文件上传
+     * @param file
+     * @ return com.zyp.pojo.Result<java.lang.String>
+     * @ author DELL
+     */
     @PostMapping("/upload")
     public Result<String> upload(MultipartFile file) throws Exception {
-        //获取上传的文件原始名称
-        String originalFilename= file.getOriginalFilename();
-        //需要保证文件名不一样，防止文件覆盖
-        //文件名称是UUID，类型从originalFilename中获取(.png或.jpg等等)
-        String filename= UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf('.'));
-        String url=AliOssUtil.upload(filename,file.getInputStream());
-        return Result.success(url);
+
+        String url = MinioUtil.upload(file, minioProperties.getEndpoint(), minioProperties.getPort(), minioProperties.getAccessKey(), minioProperties.getSecretKey(), minioProperties.getBucketName());
+        log.info("url= {}",url);
+
+        if(url.isEmpty() || url.equals("error")){
+            throw new UploadErrorException("文件上传失败");
+        }else{
+            return Result.success(url);
+        }
     }
 }
